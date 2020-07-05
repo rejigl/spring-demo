d5afc8646420d029c7ea220f723f5f9b4ef5b2e3
Reji Nair <reji.nair@us.panasonic.com>
2020-07-05 13:00:00 -0400

    Spring Demo

---
+++ src/main/java/com/rejigl/spring/demo/controllers/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.rejigl.spring.demo.controllers;
+
+import com.rejigl.spring.demo.entities.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/rest")
+    public Greeting greet(@RequestParam(required = false, defaultValue = "World") String name){
+        return new Greeting(String.format("Hello, %s!", name));
+    }
+}
